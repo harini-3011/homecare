3d7d7343c1088ef64ed9500bbf1e5fc9750b60f4
harini-3011 <118709548+harini-3011@users.noreply.github.com>
2024-02-29 16:23:06 +0530

    Add files via upload

---
+++ backend/day2/entity/Services.java
@@ -0,0 +1,42 @@
+package com.example.backend.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import jakarta.persistence.Table;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Lob;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="services")
+
+public class Services {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="Name",nullable=false)
+    private String name;
+
+    @Column(name="Description",nullable=false)
+    private String description;
+
+    @Column(name="Charge",nullable=false)
+    private Long charge;
+
+    @Lob
+    @Column(name="Image",nullable = false)
+    private byte[] image;
+
+
+}
+
+
